f48b4a52284a28e6b21f446591768a066696d95c
Shouryakothiyal <kothroxz55@gmail.com>
2015-11-20 19:00:13 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,57 @@
+<head>
+		<title>:: Home Page ::</title>
+		<link href="web/css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		
+	</head>
+	<body>
+		<!---start-wrap---->
+		
+			<!---start-header---->
+			<div class="header">
+				<div class="wrap">
+				<!---start-logo---->
+				<div class="logo">
+					<a href="index.html"><img src="web/images/logo13.JPG" title="logo" /></a>
+				</div>
+				<!---end-logo---->
+				<!---start-search---->
+				<div class="top-search-bar">
+					<div class="header-top-nav">
+				<!--		<ul>
+                            <li> Welcome Guest!  </li>
+							<li><a href="#"><input type="text" name="loginId" placeholder="Email or Phone no"/><input type="password" name="loginPass" placeholder="Password"/> Login </a></li>
+                            <li><a href="#"><img src="web/images/marker1.png" title="help" /> Help </a></li>
+							
+						
+						</ul> -->
+					</div>
+				</div>
+				<div class="clear"> </div>
+				</div>
+			</div>
+				<div class="clear"> </div>
+				<div class="header-nav">
+					<div class="wrap">
+					<div class="left-nav">
+						<ul>
+							<li><a href="./index.php">Home</a></li>
+							<li><a href="./by_year.php">Search By Year</a></li>
+						
+							<li><a href="./by_name.php">Search By Name</a></li>
+						</ul>
+					</div>
+					<div class="right-social-icons">
+						<ul>
+							<li><a class="icon1" href="#"> </a></li>
+							<li><a class="icon2" href="#"> </a></li>
+							<li><a class="icon3" href="#"> </a></li>
+							<li><a class="icon4" href="#"> </a></li>
+						</ul>
+					</div>
+					<div class="clear"> </div>
+				</div>
+				<!---start-search---->
+			</div>
+			<!---end-header---->
+</body>
+            
