50637d63356720b742f99d138dbc541d7d3c9737
Shouryakothiyal <kothroxz55@gmail.com>
2015-11-20 18:58:41 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+		<title>:: Home Page ::</title>
+		<link href="web/css/style.css" rel="stylesheet" type="text/css"  media="all" />
+</head>
+<body>
+					<div class="footer"> 
+						<div class="wrap"> 
+						<div class="footer-left">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="by_year.php">Search By Year</a></li>
+								
+								<li><a href="by_name.php">Search By Name</a></li>
+							</ul>
+						</div>
+						<div class="footer-right">
+							<p>&#169 All Rights Reserved | Design By: <a href="http://twitter.com/fetniteshrai">Nitesh Kumar Rai</a></p>
+                            <p> <a href="#">   Contact Us  </a></p>
+   						</div>
+						<div class=="clear"> </div>
+					</div>
+					<div class="clear"> </div>
+				</div>
+	</body>
+</html>
+
