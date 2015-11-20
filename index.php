e6f5c13332015ab502bc6d70b106f732435aa15f
Shouryakothiyal <kothroxz55@gmail.com>
2015-11-20 19:01:37 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Home Page</title>
+</head>
+<body align="center">
+<table   border="2px" style="width: 100%; height: 50px ; ">
+<tr><td colspan="3"> <?php
+
+                       include 'header.php';
+                        ?> </td></tr>
+</table>
+<br />
+<table style="width: 100%; height: 50%;">
+<br />
+<tr><td align=center style="font-size: 35;">BABY NAME Ranking</td></tr>
+
+
+
+</table>
+<table  border="2px" align=center style="width: 100%; height: 50px; ">
+ <tr><td colspan="3" style="padding:0,10px;"> <?php
+ 
+                                                    include 'footer.php';
+
+                                                    ?>  </td></tr>
+</table>
+</body>
+</html>
